51f7d69481cae0eb3d5a6a0b52d7dd03a9292cc8
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2020-12-19 21:43:25 +0000

    Update Main.php

--- NamelessLeaf/src/Main.php
+++ NamelessLeaf/src/Main.php
@@ -7,4 +7,20 @@ use pocketmine\event\Listener;
 use pocketmine\level\sound\BlazeShootSound;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 
-$event->setKnockback($event->getKnockback() * 1.5);
+class KnockBack extends PluginBase implements Listener{
+
+  public function onEnable(){
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  } // Registers the events listed above and used below
+
+public function onLoad(){
+$this->getLogger()->info("KnockBack §aenabled");
+} // Sends a message to console to Alert you that the plugin has been loaded
+
+  public function onDamage(EntityDamageEvent $event){
+  $entity = $event->getEntity();
+  if($event instanceof EntityDamageByEntityEvent){
+  $entity->knockBack($entity, 0, 1, 0, 1);
+  } // KnockBack in PVP
+}
+
